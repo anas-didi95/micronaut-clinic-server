2f2a61df5a9e23c9b557d7bff301003ed0890bdf
Anas Juwaidi <anas.didi95@gmail.com>
2022-07-29 21:27:01 +0800

    ADD: Add dependency for testing

--- src/test/java/com/anasdidi/clinic/domain/user/UserControllerTests.java
+++ src/test/java/com/anasdidi/clinic/domain/user/UserControllerTests.java
@@ -1,30 +1,22 @@
 package com.anasdidi.clinic.domain.user;
 
-import org.junit.jupiter.api.Assertions;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.Test;
 
-import io.micronaut.http.HttpRequest;
-import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
-import reactor.core.publisher.Mono;
+import io.restassured.specification.RequestSpecification;
 
 @MicronautTest
 class UserControllerTests {
 
-  private final HttpClient httpClient;
-
-  @Inject
-  UserControllerTests(HttpClient httpClient) {
-    this.httpClient = httpClient;
-  }
+  private static final String baseURI = "/v1/user";
 
   @Test
-  void testHello() {
-    HttpRequest<String> request = HttpRequest.GET("/v1/user");
-    Mono.from(httpClient.exchange(request)).subscribe(responseBody -> {
-      Assertions.assertNotNull(responseBody);
-      Assertions.assertEquals("Hello world", responseBody);
-    });
+  void testHello(RequestSpecification spec) {
+    spec
+        .given().when().get(baseURI)
+        .then().statusCode(HttpStatus.OK.getCode()).body(is("Hello world"));
   }
 }
