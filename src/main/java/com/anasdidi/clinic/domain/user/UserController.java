01349378bc1e31dadc6c2e3426b99ff0c85d3bb1
Anas Juwaidi <anas.didi95@gmail.com>
2022-08-22 20:02:00 +0800

    ADD: Add swagger for user controller create

--- src/main/java/com/anasdidi/clinic/domain/user/UserController.java
+++ src/main/java/com/anasdidi/clinic/domain/user/UserController.java
@@ -1,5 +1,7 @@
 package com.anasdidi.clinic.domain.user;
 
+import com.anasdidi.clinic.exception.ErrorResponseDTO;
+
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
@@ -8,6 +10,10 @@ import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Put;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import reactor.core.publisher.Mono;
@@ -15,10 +21,34 @@ import reactor.core.publisher.Mono;
 interface UserController {
 
   @Post(value = "/", consumes = { MediaType.APPLICATION_JSON }, produces = { MediaType.APPLICATION_JSON })
-  @Operation(description = "Create User")
   @Tag(name = "user")
-  @ApiResponse(responseCode = "201", description = "Created")
-  Mono<HttpResponse<UserDTO>> createUser(@Body UserDTO requestBody);
+  @Operation(description = "Create User")
+  @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(type = "object", allOf = UserDTO.class), examples = {
+      @ExampleObject("""
+          {
+             "id": "anas",
+             "createdDate": 1661084378.897929437,
+             "createdBy": "SYSTEM",
+             "updatedDate": 1661084378.897929437,
+             "version": 0,
+             "fullName": "Anas Juwaidi"
+          }
+          """) }))
+  @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(type = "object", allOf = ErrorResponseDTO.class), examples = {
+      @ExampleObject("""
+          {
+            "code": "E002",
+            "message": "Record [anas] already exists!",
+            "traceId": "220821122013X0nu6YXL"
+          }
+          """) }))
+  Mono<HttpResponse<UserDTO>> createUser(@RequestBody(content = @Content(examples = {
+      @ExampleObject("""
+          {
+            "id": "anas",
+            "fullName": "Anas Juwaidi"
+          }
+          """) })) @Body UserDTO requestBody);
 
   @Put(value = "/{id}", consumes = { MediaType.APPLICATION_JSON }, produces = { MediaType.APPLICATION_JSON })
   @Operation(description = "Update User")
