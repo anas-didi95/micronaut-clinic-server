704611975be3c4c43230ee1603fe4d66c48590aa
Anas Juwaidi <anas.didi95@gmail.com>
2022-08-30 21:00:29 +0800

    REF: Refactor code

--- src/main/java/com/anasdidi/clinic/common/SearchDTO.java
+++ src/main/java/com/anasdidi/clinic/common/SearchDTO.java
@@ -2,6 +2,7 @@ package com.anasdidi.clinic.common;
 
 import java.util.List;
 
+import io.micronaut.data.model.Page;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,16 +21,19 @@ public class SearchDTO<T> {
   private List<T> resultList;
   private PaginationDTO pagination;
 
-  @NoArgsConstructor
-  @AllArgsConstructor
-  @Builder
   @Getter
-  @Setter
   @ToString
   public static class PaginationDTO {
 
     private int totalPages;
     private int pageNumber;
     private int size;
+
+    @SuppressWarnings("rawtypes")
+    public PaginationDTO(Page page) {
+      this.totalPages = page.getTotalPages();
+      this.pageNumber = page.getPageNumber();
+      this.size = page.getSize();
+    }
   }
 }
--- src/main/java/com/anasdidi/clinic/domain/user/UserDataFetcher.java
+++ src/main/java/com/anasdidi/clinic/domain/user/UserDataFetcher.java
@@ -1,16 +1,11 @@
 package com.anasdidi.clinic.domain.user;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.anasdidi.clinic.common.SearchDTO;
 
-import graphql.GraphQLContext;
 import graphql.schema.DataFetcher;
 import io.micronaut.data.model.Pageable;
 import jakarta.inject.Inject;
@@ -19,7 +14,6 @@ import jakarta.inject.Singleton;
 @Singleton
 public class UserDataFetcher {
 
-  private final Logger logger = LoggerFactory.getLogger(UserDataFetcher.class);
   private final UserRepository userRepository;
 
   @Inject
@@ -27,29 +21,14 @@ public class UserDataFetcher {
     this.userRepository = userRepository;
   }
 
-  public DataFetcher<CompletableFuture<SearchDTO<UserDTO>>> getUserList() {
+  public DataFetcher<CompletableFuture<SearchDTO<UserDTO>>> getUserSearch() {
     return (env) -> {
-      logger.debug("[{}:userList] START", env.getExecutionId());
-      Pageable pageable = Pageable.from(0, 200);
+      Pageable pageable = Pageable.from(0, 10);
       return userRepository.findAll(pageable).map(result -> {
-        Map<String, Object> pagination = Map.of("totalPages", result.getTotalPages());
-        logger.debug("pagination={}", pagination);
-        logger.debug("getNumberOfElements={}", result.getNumberOfElements());
-        logger.debug("totalPages={}", result.getTotalPages());
-        logger.debug("pageNumber={}", result.getPageNumber());
-        logger.debug("getSize={}", result.getSize());
-        logger.debug("getPageable().getNumber={}", result.getPageable().getNumber());
-        logger.debug("getPageable().getSize={}", result.getPageable().getSize());
-        GraphQLContext context = env.getContext();
-        System.out.println("eontext.test=" + context.get("test"));
-        context.put("test", "hello world 1");
-        context.put("pagination", pagination);
         List<UserDTO> resultList = result.getContent().stream().map(UserUtils::copy).collect(Collectors.toList());
         return SearchDTO.<UserDTO>builder()
             .resultList(resultList)
-            .pagination(SearchDTO.PaginationDTO.builder()
-                .totalPages(result.getTotalPages())
-                .build())
+            .pagination(new SearchDTO.PaginationDTO(result))
             .build();
       }).toFuture();
     };
