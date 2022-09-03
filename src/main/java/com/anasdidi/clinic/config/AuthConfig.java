fdc80b6f46f392a1448dd70207e7b1e39538d49c
Anas Juwaidi <anas.didi95@gmail.com>
2022-09-03 20:53:16 +0800

    ADD: Add sample code

---
+++ src/main/java/com/anasdidi/clinic/config/AuthConfig.java
@@ -0,0 +1,27 @@
+package com.anasdidi.clinic.config;
+
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.security.authentication.AuthenticationProvider;
+import io.micronaut.security.authentication.AuthenticationResponse;
+import jakarta.inject.Singleton;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+@Factory
+public class AuthConfig {
+
+  @Singleton
+  public AuthenticationProvider authenticationProvider() {
+    return (httpRequest, authenticationRequest) -> {
+      return Flux.create(emitter -> {
+        if (authenticationRequest.getIdentity().equals("sherlock") &&
+            authenticationRequest.getSecret().equals("password")) {
+          emitter.next(AuthenticationResponse.success((String) authenticationRequest.getIdentity()));
+          emitter.complete();
+        } else {
+          emitter.error(AuthenticationResponse.exception());
+        }
+      }, FluxSink.OverflowStrategy.ERROR);
+    };
+  }
+}
---
+++ src/test/java/com/anasdidi/clinic/domain/auth/AuthControllerTests.java
@@ -0,0 +1,64 @@
+package com.anasdidi.clinic.domain.auth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.text.ParseException;
+
+import org.junit.jupiter.api.Test;
+
+import com.nimbusds.jwt.JWTParser;
+import com.nimbusds.jwt.SignedJWT;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.security.authentication.UsernamePasswordCredentials;
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+
+@MicronautTest
+public class AuthControllerTests {
+
+  @Inject
+  @Client("/")
+  HttpClient client;
+
+  @Test
+  void accessingASecuredUrlWithoutAuthenticatingReturnsUnauthorized() {
+    HttpClientResponseException e = assertThrows(HttpClientResponseException.class, () -> {
+      client.toBlocking().exchange(HttpRequest.GET("/clinic/").accept(MediaType.TEXT_PLAIN));
+    });
+
+    assertEquals(HttpStatus.UNAUTHORIZED, e.getStatus());
+  }
+
+  @Test
+  void uponSuccessfulAuthenticationAJsonWebTokenIsIssuedToTheUser() throws ParseException {
+    UsernamePasswordCredentials creds = new UsernamePasswordCredentials("sherlock", "password");
+    HttpRequest<?> request = HttpRequest.POST("/clinic/login", creds);
+    HttpResponse<BearerAccessRefreshToken> rsp = client.toBlocking().exchange(request, BearerAccessRefreshToken.class);
+    assertEquals(HttpStatus.OK, rsp.getStatus());
+
+    BearerAccessRefreshToken bearerAccessRefreshToken = rsp.body();
+    assertEquals("sherlock", bearerAccessRefreshToken.getUsername());
+    assertNotNull(bearerAccessRefreshToken.getAccessToken());
+    assertTrue(JWTParser.parse(bearerAccessRefreshToken.getAccessToken()) instanceof SignedJWT);
+
+    String accessToken = bearerAccessRefreshToken.getAccessToken();
+    HttpRequest<?> requestWithAuthorization = HttpRequest.GET("/clinic/")
+        .accept(MediaType.TEXT_PLAIN)
+        .bearerAuth(accessToken);
+    HttpResponse<String> response = client.toBlocking().exchange(requestWithAuthorization, String.class);
+
+    assertEquals(HttpStatus.OK, rsp.getStatus());
+    assertEquals("sherlock", response.body());
+  }
+}
