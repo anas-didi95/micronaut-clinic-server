22c989d00d84a6ba61d2bd411246d71dc9d07112
Anas Juwaidi <anas.didi95@gmail.com>
2022-09-13 20:55:44 +0800

    ADD: Add test refresh token validation

--- src/test/java/com/anasdidi/clinic/domain/auth/AuthControllerTests.java
+++ src/test/java/com/anasdidi/clinic/domain/auth/AuthControllerTests.java
@@ -6,12 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.text.ParseException;
+import java.util.Map;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
 import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.jwt.SignedJWT;
 
+import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
@@ -20,6 +23,7 @@ import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.security.authentication.UsernamePasswordCredentials;
+import io.micronaut.security.token.jwt.endpoints.TokenRefreshRequest;
 import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
@@ -73,5 +77,28 @@ public class AuthControllerTests {
     assertNotNull(rsp.getRefreshToken());
 
     assertTrue(JWTParser.parse(rsp.getAccessToken()) instanceof SignedJWT);
+  }
+
+  @Test
+  @SuppressWarnings("rawtypes")
+  void accessingSecuredURLWithoutAuthenticatingReturnsUnauthorized() {
+    String unsignedRefreshedToken = "foo";
+    Argument<BearerAccessRefreshToken> bodyArgument = Argument.of(BearerAccessRefreshToken.class);
+    Argument<Map> errorArgument = Argument.of(Map.class);
+
+    HttpClientResponseException e = assertThrows(HttpClientResponseException.class, () -> {
+      client.toBlocking().exchange(
+          HttpRequest.POST("/clinic/oauth/access_token", new TokenRefreshRequest(unsignedRefreshedToken)),
+          bodyArgument,
+          errorArgument);
+    });
+    assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
+
+    Optional<Map> mapOptional = e.getResponse().getBody(Map.class);
+    assertTrue(mapOptional.isPresent());
+
+    Map m = mapOptional.get();
+    assertEquals("invalid_grant", m.get("error"));
+    assertEquals("Refresh token is invalid", m.get("error_description"));
   }
 }
