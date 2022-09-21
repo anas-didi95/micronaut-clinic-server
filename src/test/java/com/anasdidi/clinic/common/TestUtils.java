22d421cd94ca5b4b131e10f3adcf8805e3d1cf9d
Anas Juwaidi <anas.didi95@gmail.com>
2022-09-21 17:22:58 +0800

    REF: Refactor utils

--- src/test/java/com/anasdidi/clinic/common/TestUtils.java
+++ src/test/java/com/anasdidi/clinic/common/TestUtils.java
@@ -12,14 +12,11 @@ public final class TestUtils {
 
   public static final String getAccessToken(AppClient appClient) {
     Map<String, Object> testMap = TestConstants.testMap;
-    String accessToken = (String) testMap.get("accessToken");
-    if (accessToken == null) {
+    return (String) testMap.computeIfAbsent("accessToken", value -> {
       User user = TestConstants.User.ADMIN1;
       UsernamePasswordCredentials creds = new UsernamePasswordCredentials(user.id, user.password);
       BearerAccessRefreshToken loginRsp = appClient.login(creds);
-      accessToken = loginRsp.getAccessToken();
-      testMap.put("accessToken", accessToken);
-    }
-    return accessToken;
+      return loginRsp.getAccessToken();
+    });
   }
 }
