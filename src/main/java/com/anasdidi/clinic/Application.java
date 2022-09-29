a157b9d4a8c9a7989fd20c4e2ab017f7d8a1967e
Anas Juwaidi <anas.didi95@gmail.com>
2022-09-29 20:33:27 +0800

    ADD: Add openapi security scheme

--- src/main/java/com/anasdidi/clinic/Application.java
+++ src/main/java/com/anasdidi/clinic/Application.java
@@ -2,16 +2,23 @@ package com.anasdidi.clinic;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 
 @OpenAPIDefinition(info = @Info(//
     title = "Micronaut Clinic Microservice", //
     version = "0.2.0", //
     description = "Micronaut Clinic Microservice", //
     license = @License(name = "Apache 2.0", url = "http://www.apache.org/licenses"), //
-    contact = @Contact(url = "https://anasdidi.dev", name = "Anas Juwaidi", email = "anas.didi95@gmail.com")))
+    contact = @Contact(url = "https://anasdidi.dev", name = "Anas Juwaidi", email = "anas.didi95@gmail.com")), security = @SecurityRequirement(name = "jwt"))
+@SecuritySchemes({
+    @SecurityScheme(name = "jwt", type = SecuritySchemeType.HTTP, scheme = "bearer", paramName = "Authorization")
+})
 public class Application {
 
   static {
