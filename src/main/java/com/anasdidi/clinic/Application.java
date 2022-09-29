5ddcfa2b34a0dfd631630fd52545c1051fced2b1
Anas Juwaidi <anas.didi95@gmail.com>
2022-09-29 20:47:10 +0800

    ADD: Add openapi server

--- src/main/java/com/anasdidi/clinic/Application.java
+++ src/main/java/com/anasdidi/clinic/Application.java
@@ -9,6 +9,9 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
+import io.swagger.v3.oas.annotations.servers.Servers;
 
 @OpenAPIDefinition(info = @Info(//
     title = "Micronaut Clinic Microservice", //
@@ -16,6 +19,11 @@ import io.swagger.v3.oas.annotations.security.SecuritySchemes;
     description = "Micronaut Clinic Microservice", //
     license = @License(name = "Apache 2.0", url = "http://www.apache.org/licenses"), //
     contact = @Contact(url = "https://anasdidi.dev", name = "Anas Juwaidi", email = "anas.didi95@gmail.com")), security = @SecurityRequirement(name = "jwt"))
+@Servers({
+    @Server(url = "http://localhost:{port}", description = "Localhost", variables = {
+        @ServerVariable(name = "port", allowableValues = { "8080", "8081", "8082" }, defaultValue = "8080")
+    })
+})
 @SecuritySchemes({
     @SecurityScheme(name = "jwt", type = SecuritySchemeType.HTTP, scheme = "bearer", paramName = "Authorization")
 })
